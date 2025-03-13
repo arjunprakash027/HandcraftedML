e93ddb827a12b85162850ad34c316b7d9060e526
Arjun <arjunprakash027@gmail.com>
2025-03-13 09:29:38 +0530

    gaussian nb from scratch

---
+++ ground_ups/naive_bayes/gaussian_naive_bayes.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+
+def fake_data() -> None:
+    data = {
+    "Feature 1": [5.1, 4.9, 5.0, 6.5, 6.3, 6.6, 7.2, 6.9, 6.8, 5.5, 5.7, 5.8, 7.3, 7.1, 7.0],
+    "Feature 2": [3.5, 3.0, 3.4, 3.0, 2.9, 3.0, 3.2, 3.1, 3.0, 2.4, 2.6, 2.7, 2.9, 3.0, 3.1],
+    "Feature 3": [1.4, 1.4, 1.5, 4.5, 4.3, 4.6, 6.0, 5.4, 5.5, 3.8, 3.5, 3.9, 6.3, 5.9, 5.8],
+    "Feature 4": [0.2, 0.2, 0.2, 1.5, 1.3, 1.4, 2.3, 2.1, 2.2, 1.1, 1.0, 1.2, 2.4, 2.1, 2.2],
+    "Feature 5": [0.5, 0.6, 0.4, 1.2, 1.1, 1.3, 2.5, 2.3, 2.4, 0.9, 1.0, 1.2, 2.8, 2.6, 2.7],
+    "Class": [0, 0, 0, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2]
+    }
+
+    df = pd.DataFrame(data) 
+
+    return df
+class GaussianNB:
+
+    def __init__(self, df:pd.DataFrame):
+        self.df = df
+        #This dict holds all the neccessary statistics 
+        self.stats = {}
+
+    def fit(self):
+        
+        total = self.df.shape[0]
+        
+        for class_vals in self.df['Class'].unique():
+
+            class_df = self.df[self.df['Class'] == class_vals ]
+            class_df = class_df.drop("Class",axis=1)
+
+            class_prob = class_df.shape[0] / total
+
+            feature_vals = []            
+            for features in class_df.columns:
+                feature_list = self.df[features].to_numpy()
+                mean = np.mean(feature_list)
+                std_dv = np.std(feature_list)
+
+                feature_vals.append(    
+                    {
+                        features:{
+                        "mean":mean,
+                        "std_dv":std_dv
+                    }
+                    }
+                )
+
+            self.stats[class_vals] = {
+                'class_prob':class_prob,
+                "feature_vals":feature_vals
+            }
+
+    def predict(self):
+        pass
+
+
+if __name__ in '__main__':
+    df = fake_data()
+    gnb = GaussianNB(df=df)
+    gnb.fit()
+    print(gnb.stats)
